77fad52f0fda2893566e2a07a75f773cbb896194
sunil <esunil.pawar@gmail.com>
2015-04-23 15:17:10 +0530

    fix for #487

--- CRM/Contribute/Form/Contribution/Main.php
+++ CRM/Contribute/Form/Contribution/Main.php
@@ -877,6 +877,7 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
       if ($self->_useForMember == 1 && !empty($check) && $membershipIsActive) {
         $priceFieldIDS = array();
         $priceFieldMemTypes = array();
+        $priceFieldIsRequired = array();
 
         foreach ($self->_priceSet['fields'] as $priceId => $value) {
           if (!empty($fields['price_' . $priceId]) || ($self->_quickConfig && $value['name'] == 'membership_amount' && empty($self->_membershipBlock['is_required']))) {
@@ -901,6 +902,7 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
                   )
                 ) {
                   $priceFieldMemTypes[] = $val['membership_type_id'];
+                  $priceFieldIsRequired[$priceId] = $value['is_required'];
                 }
               }
             }
@@ -930,8 +932,8 @@ class CRM_Contribute_Form_Contribution_Main extends CRM_Contribute_Form_Contribu
             }
           }
         }
-
-        if (empty($priceFieldMemTypes)) {
+        $priceFieldIsRequired = array_filter($priceFieldIsRequired);
+        if (empty($priceFieldMemTypes) && !empty($priceFieldIsRequired)) {
           $errors['_qf_default'] = ts('Please select at least one membership option.');
         }
       }

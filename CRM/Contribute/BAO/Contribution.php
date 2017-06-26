f0a49a4651152b5211d92ce720a3b48c0e89b566
yashodha <yashodha@cividesk.com>
2017-06-26 15:40:36 +0530

    assign membership type id to template

--- CRM/Contribute/BAO/Contribution.php
+++ CRM/Contribute/BAO/Contribution.php
@@ -2627,6 +2627,7 @@ INNER JOIN civicrm_activity ON civicrm_activity_contact.activity_id = civicrm_ac
             $template->assign('membership_name',
               CRM_Member_PseudoConstant::membershipType($membership->membership_type_id)
             );
+            $template->assign('mem_type_id', $membership->membership_type_id);
             $template->assign('mem_start_date', $membership->start_date);
             $template->assign('mem_join_date', $membership->join_date);
             $template->assign('mem_end_date', $membership->end_date);

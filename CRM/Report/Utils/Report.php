21f97b43f4096c3a159e7019d606b53c16066e89
Nicolas Ganivet <nicolas@cividesk.com>
2017-07-30 00:18:19 +0800

    Fixes #2875 Accentuated characters incorrect in exported CSV files

--- CRM/Report/Utils/Report.php
+++ CRM/Report/Utils/Report.php
@@ -230,6 +230,11 @@ WHERE  inst.report_id = %1";
     // and it continues to work on Libre Office, Numbers, Notes etc.
     $csv = "\xEF\xBB\xBF";
 
+    // Add BOM for additional compatibility with Excel 2007 SP3 or more
+    // cf. http://stackoverflow.com/questions/155097/microsoft-excel-mangles-diacritics-in-csv-files
+    // and http://www.unicode.org/faq/utf_bom.html#bom1
+    $csv .= "\xEF\xBB\xBF";
+
     // Add headers if this is the first row.
     $columnHeaders = array_keys($form->_columnHeaders);
 

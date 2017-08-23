301207c41b1138b6e44bae52adb597fb69a083be
yashodha <yashodha@cividesk.com>
2017-08-23 17:01:35 +0530

    fix for event payment processor for checkboxes

--- CRM/Event/Form/ManageEvent/Fee.php
+++ CRM/Event/Form/ManageEvent/Fee.php
@@ -560,7 +560,7 @@ class CRM_Event_Form_ManageEvent_Fee extends CRM_Event_Form_ManageEvent {
     }
 
     if (!empty($params['payment_processor'])) {
-      $params['payment_processor'] = str_replace(',', CRM_Core_DAO::VALUE_SEPARATOR, $params['payment_processor']);
+      $params['payment_processor'] = implode(CRM_Core_DAO::VALUE_SEPARATOR, array_keys($params['payment_processor']));
     }
     else {
       $params['payment_processor'] = 'null';

54356e706fa0adaa2a026080d9e47ed3ad9aa75e
yashodha <yashodha@cividesk.com>
2017-10-26 12:14:05 +0530

    security fixes

--- CRM/Report/Page/InstanceList.php
+++ CRM/Report/Page/InstanceList.php
@@ -85,8 +85,11 @@ class CRM_Report_Page_InstanceList extends CRM_Core_Page {
   public function info() {
 
     $report = '';
+    $queryParams = array();
+
     if ($this->ovID) {
-      $report .= " AND v.id = {$this->ovID} ";
+      $report .= " AND v.id = %1 ";
+      $queryParams[1] = array($this->ovID, 'Integer');
     }
 
     if ($this->compID) {
@@ -95,7 +98,8 @@ class CRM_Report_Page_InstanceList extends CRM_Core_Page {
         $this->_compName = 'Contact';
       }
       else {
-        $report .= " AND v.component_id = {$this->compID} ";
+        $report .= " AND v.component_id = %2 ";
+        $queryParams[2] = array($this->compID, 'Integer');
         $cmpName = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_Component', $this->compID,
           'name', 'id'
         );
@@ -106,10 +110,12 @@ class CRM_Report_Page_InstanceList extends CRM_Core_Page {
       }
     }
     elseif ($this->grouping) {
-      $report .= " AND v.grouping = '{$this->grouping}' ";
+      $report .= " AND v.grouping = %3 ";
+      $queryParams[3] = array($this->grouping, 'String');
     }
     elseif ($this->myReports) {
-      $report .= " AND inst.owner_id = " . CRM_Core_Session::getLoggedInContactID();
+      $report .= " AND inst.owner_id = %4 ";
+      $queryParams[4] = array(CRM_Core_Session::getLoggedInContactID(), 'Integer');
     }
 
     $sql = "

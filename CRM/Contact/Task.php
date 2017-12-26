a8cf4b047fdf43cef958e1c1d377ea172f27a94d
yashodha <yashodha@cividesk.com>
2017-12-26 10:34:25 +0530

    fix verbiage to be more clear

--- CRM/Contact/Task.php
+++ CRM/Contact/Task.php
@@ -48,7 +48,8 @@ class CRM_Contact_Task extends CRM_Core_Task {
     RESTORE = 108,
     COMMUNICATION_PREFS = 109,
     INDIVIDUAL_CONTACTS = 110,
-    ADD_TO_CASE = 111;
+    ADD_TO_CASE = 111,
+    CREATE_MAILING_TRADITIONAL = 112;
 
   static $objectType = 'contact';
 
@@ -246,6 +247,16 @@ class CRM_Contact_Task extends CRM_Core_Task {
           'class' => 'CRM_Mailing_Form_Task_AdhocMailing',
           'result' => FALSE,
         );
+
+        $templateTypes = CRM_Mailing_BAO_Mailing::getTemplateTypes();
+        if (count($templateTypes) > 1) {
+          self::$_tasks[self::CREATE_MAILING]['title'] = ts('Email - schedule/send via Mosaico CiviMail');
+          self::$_tasks[self::CREATE_MAILING_TRADITIONAL] = array(
+            'title' => ts('Email - schedule/send via traditional CiviMail'),
+            'class' => 'CRM_Mailing_Form_Task_AdhocMailing',
+            'result' => FALSE,
+          );
+        }
       }
 
       if (CRM_Core_Permission::access('CiviCase')) {

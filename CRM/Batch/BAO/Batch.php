0c7492e546d0d6c13e05706fb150e92b4841dc04
yashodha <yashodha@cividesk.com>
2018-01-23 16:40:20 +0530

    fix search for fields based on multiple values

--- CRM/Batch/BAO/Batch.php
+++ CRM/Batch/BAO/Batch.php
@@ -748,6 +748,36 @@ LEFT JOIN civicrm_contribution_soft ON civicrm_contribution_soft.contribution_id
           $values['contribution_date_low'] = $date['from'];
           $values['contribution_date_high'] = $date['to'];
         }
+
+        $specialParams = array(
+          'financial_type_id',
+          'contribution_soft_credit_type_id',
+          'contribution_status_id',
+          'contribution_trxn_id',
+          'contribution_page_id',
+          'contribution_product_id',
+          'invoice_id',
+          'payment_instrument_id',
+          'contribution_batch_id',
+        );
+
+        foreach ($specialParams as $key) {
+          if (($val = CRM_Utils_Array::value($key, $values)) && !is_array($val)) {
+            $values[$key] = explode(',', $val);
+          }
+        }
+        CRM_Contact_BAO_Query::processSpecialFormValue($values, $specialParams);
+        $searchParams = CRM_Contact_BAO_Query::convertFormValues($values);
+        // @todo the use of defaultReturnProperties means the search will be inefficient
+        // as slow-unneeded properties are included.
+        $query = new CRM_Contact_BAO_Query($searchParams,
+          CRM_Contribute_BAO_Query::defaultReturnProperties(CRM_Contact_BAO_Query::MODE_CONTRIBUTE,
+            FALSE
+          ), NULL, FALSE, FALSE, CRM_Contact_BAO_Query::MODE_CONTRIBUTE
+        );
+        if ($field == 'contribution_date_high' || $field == 'contribution_date_low') {
+          $query->dateQueryBuilder($params[$field], 'civicrm_contribution', 'contribution_date', 'receive_date', 'Contribution Date');
+        }
       }
     }
 

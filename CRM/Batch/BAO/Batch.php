cd933934b13b36b0e1cdcb50fd52656c45a203db
yashodha <yashodha@cividesk.com>
2018-01-24 18:58:43 +0530

    make sure payment method search works for batch as well

--- CRM/Batch/BAO/Batch.php
+++ CRM/Batch/BAO/Batch.php
@@ -676,6 +676,7 @@ LEFT JOIN civicrm_contribution_soft ON civicrm_contribution_soft.contribution_id
       'financial_type_id',
       'contribution_page_id',
       'contribution_payment_instrument_id',
+      'payment_instrument',
       'contribution_trxn_id',
       'contribution_source',
       'contribution_currency_type',
@@ -732,7 +733,7 @@ LEFT JOIN civicrm_contribution_soft ON civicrm_contribution_soft.contribution_id
           'contribution_page_id',
           'contribution_product_id',
           'invoice_id',
-          'payment_instrument_id',
+          'payment_instrument',
           'contribution_batch_id',
         );
 
@@ -741,7 +742,10 @@ LEFT JOIN civicrm_contribution_soft ON civicrm_contribution_soft.contribution_id
             $values[$key] = explode(',', $val);
           }
         }
-        CRM_Contact_BAO_Query::processSpecialFormValue($values, $specialParams);
+
+        $changeNames = array('payment_instrument_id' => 'payment_instrument');
+        CRM_Contact_BAO_Query::processSpecialFormValue($values, $specialParams, $changeNames);
+
         $searchParams = CRM_Contact_BAO_Query::convertFormValues($values);
         // @todo the use of defaultReturnProperties means the search will be inefficient
         // as slow-unneeded properties are included.

0bf2f0dd31bcbb97f3c33b8d30f8ad0cceb88b86
Sunil Pawar <sunil@cividesk.com>
2018-02-09 00:35:25 +0530

    show valid contact only

--- CRM/Report/Form/Contact/Summary.php
+++ CRM/Report/Form/Contact/Summary.php
@@ -163,6 +163,11 @@ class CRM_Report_Form_Contact_Summary extends CRM_Report_Form {
     $this->joinCountryFromAddress();
   }
 
+  public function where() {
+    parent::where();
+    $this->_where .= " AND {$this->_aliases['civicrm_contact']}.is_deleted = 0 ";
+  }
+
   public function postProcess() {
 
     $this->beginPostProcess();

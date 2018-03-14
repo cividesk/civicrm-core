d17024fccfcf16776e68d9074c94af7ad6862a34
Sunil Pawar <sunil@cividesk.com>
2018-03-14 14:57:37 +0530

    civicrm upgrade fixed code conflict

--- CRM/Price/BAO/PriceField.php
+++ CRM/Price/BAO/PriceField.php
@@ -161,7 +161,6 @@ class CRM_Price_BAO_PriceField extends CRM_Price_DAO_PriceField {
           throw new CRM_Core_Exception($e->getMessage());
         }
       }
-      */
     }
 
     $transaction->commit();

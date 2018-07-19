abaaced68db568fe1f4554073b1a9ed66b874c44
Sunil Pawar <sunil@cividesk.com>
2018-07-19 12:23:54 +0530

    Fix issue with duplicate entry in uf match table because of onbehalf details available in dedupe function

--- CRM/Core/BAO/UFMatch.php
+++ CRM/Core/BAO/UFMatch.php
@@ -181,11 +181,18 @@ class CRM_Core_BAO_UFMatch extends CRM_Core_DAO_UFMatch {
       if (!empty($_POST) && !$isLogin) {
         $params = $_POST;
         $params['email'] = $uniqId;
+
         // dev/core#1858 Ensure that if we have a contactID parameter which is set in the Create user Record contact task form
         // That this contacID value is passed through as the contact_id to the get duplicate contacts function. This is necessary because for Drupal 8 this function gets invoked
         // Before the civicrm_uf_match record is added where as in D7 it isn't called until the user tries to actually login.
         if (!empty($params['contactID'])) {
           $params['contact_id'] = $params['contactID'];
+
+        // unset onbehalf array
+        // dedupe function flatten the array then rename onbehalf 'email-3', 'email-Primary' TO 'email' which overwrite Individual email
+        // because of this dedupe for individual not work properly and return empty array
+        if (array_key_exists('onbehalf', $params)) {
+          unset($params['onbehalf']);
         }
 
         $ids = CRM_Contact_BAO_Contact::getDuplicateContacts($params, 'Individual', 'Unsupervised', [], FALSE);

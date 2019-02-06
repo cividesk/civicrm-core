0118297908de67ebd3a72db56c30edfa6bfd3377
Nicolas Ganivet <nicolas@cividesk.com>
2019-02-06 14:02:17 -0700

    #5587: incorrect Participant Role filter in report

--- CRM/Report/Form/Event/ParticipantListing.php
+++ CRM/Report/Form/Event/ParticipantListing.php
@@ -556,7 +556,7 @@ ORDER BY  cv.label
                   $operator = 'NOT';
                 }
 
-                $regexp = "[[:cntrl:]]*" . implode('[[:>:]]*|[[:<:]]*', $value) . "[[:cntrl:]]*";
+                $regexp = "([[:cntrl:]]|^)" . implode('([[:cntrl:]]|$)|([[:cntrl:]]|^)', (array) $value) . "([[:cntrl:]]|$)";
                 $clause = "{$field['dbAlias']} {$operator} REGEXP '{$regexp}'";
               }
               $op = NULL;

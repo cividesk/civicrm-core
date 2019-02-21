ed397b7571dbd70d2e76fc76758bd09edaaeb9e7
root <root@rudra.ops.cividesk.com>
2019-02-21 08:25:55 +0000

    check for mysql Exception, arise due to foreign key constraint

--- CRM/Core/BAO/SchemaHandler.php
+++ CRM/Core/BAO/SchemaHandler.php
@@ -332,8 +332,14 @@ ALTER TABLE {$tableName}
         break;
     }
 
-    // CRM-7007: do not i18n-rewrite this query
-    CRM_Core_DAO::executeQuery($sql, [], TRUE, NULL, FALSE, FALSE);
+    try {
+      // CRM-7007: do not i18n-rewrite this query
+      $dao = CRM_Core_DAO::executeQuery($sql, [], TRUE, NULL, FALSE, FALSE);
+      $dao->free();
+    }
+    catch (Exception $e) {
+      $e->getMessage();
+    }
 
     $config = CRM_Core_Config::singleton();
     if ($config->logging) {

b706e97807a41ab16a0c0b7082a79bfaeecd4712
yashodha <yashodha@cividesk.com>
2019-02-26 19:39:37 +0530

    fix XSS in crmMoney plugin

--- CRM/Utils/Money.php
+++ CRM/Utils/Money.php
@@ -96,6 +96,12 @@ class CRM_Utils_Money {
       $currency = $config->defaultCurrency;
     }
 
+    // ensure $currency is a valid currency code
+    // for backwards-compatibility, also accept one space instead of a currency
+    if ($currency != ' ' && !array_key_exists($currency, self::$_currencySymbols)) {
+      throw new CRM_Core_Exception("Invalid currency \"{$currency}\"");
+    }
+
     // money_format() exists only in certain PHP install (CRM-650)
     // setlocale() affects native gettext (CRM-11054, CRM-9976)
     if (is_numeric($amount) && function_exists('money_format')) {

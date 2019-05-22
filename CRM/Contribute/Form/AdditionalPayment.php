2e79f6bee476080da570cda83da35fa328d6bc0c
sushantpaste <sushant@osseed.com>
2019-05-22 15:48:55 +0530

    Added changes from dev/core/#299

--- CRM/Contribute/Form/AdditionalPayment.php
+++ CRM/Contribute/Form/AdditionalPayment.php
@@ -181,7 +181,7 @@ class CRM_Contribute_Form_AdditionalPayment extends CRM_Contribute_Form_Abstract
       $defaults['total_amount'] = CRM_Contribute_BAO_Contribution_Utils::formatAmount(CRM_Utils_Money::format(abs($this->_refund), NULL, NULL, TRUE));
     }
     elseif ($this->_owed) {
-      $defaults['total_amount'] = CRM_Contribute_BAO_Contribution_Utils::formatAmount($this->_owed);
+      $defaults['total_amount'] = CRM_Utils_Money::formatLocaleNumericRoundedForDefaultCurrency($this->_owed);
     }
 
     // Set $newCredit variable in template to control whether link to credit card mode is included

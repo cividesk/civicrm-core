fc9944cf07f79b5e9c18343fc6218f61b4d86477
sushantpaste <sushant@osseed.com>
2019-06-19 18:17:39 +0530

    Backport changes to support tutorial extension

--- CRM/Core/BAO/Navigation.php
+++ CRM/Core/BAO/Navigation.php
@@ -328,6 +328,77 @@ FROM civicrm_navigation WHERE domain_id = $domainID";
   }
 
   /**
+   * Check if a menu item should be visible based on permissions and component.
+   *
+   * @param $item
+   * @return bool
+   */
+  public static function checkPermission($item) {
+    if (!empty($item['permission'])) {
+      $permissions = explode(',', $item['permission']);
+      $operator = CRM_Utils_Array::value('operator', $item);
+      $hasPermission = FALSE;
+      foreach ($permissions as $key) {
+        $key = trim($key);
+        $showItem = TRUE;
+        //get the component name from permission.
+        $componentName = CRM_Core_Permission::getComponentName($key);
+        if ($componentName) {
+          if (!in_array($componentName, CRM_Core_Config::singleton()->enableComponents) ||
+            !CRM_Core_Permission::check($key)
+          ) {
+            $showItem = FALSE;
+            if ($operator == 'AND') {
+              return FALSE;
+            }
+          }
+          else {
+            $hasPermission = TRUE;
+          }
+        }
+        elseif (!CRM_Core_Permission::check($key)) {
+          $showItem = FALSE;
+          if ($operator == 'AND') {
+            return FALSE;
+          }
+        }
+        else {
+          $hasPermission = TRUE;
+        }
+      }
+      if (empty($showItem) && !$hasPermission) {
+        return FALSE;
+      }
+    }
+    return TRUE;
+  }
+
+  /**
+   * Turns relative URLs (like civicrm/foo/bar) into fully-formed
+   * ones (i.e. example.com/wp-admin?q=civicrm/dashboard).
+   *
+   * If the URL is already fully-formed, nothing will be done.
+   *
+   * @param string $url
+   *
+   * @return string
+   */
+  public static function makeFullyFormedUrl($url) {
+    if (self::isNotFullyFormedUrl($url)) {
+      //CRM-7656 --make sure to separate out url path from url params,
+      //as we'r going to validate url path across cross-site scripting.
+      $path = parse_url($url, PHP_URL_PATH);
+      $q = parse_url($url, PHP_URL_QUERY);
+      $fragment = parse_url($url, PHP_URL_FRAGMENT);
+      return CRM_Utils_System::url($path, $q, FALSE, $fragment);
+    }
+    if (strpos($url, '&amp;') === FALSE) {
+      return htmlspecialchars($url);
+    }
+    return $url;
+  }
+
+  /**
    * @param array $nodes
    *   Each key is a numeral; each value is a node in
    *   the menu tree (with keys "child" and "attributes").
@@ -431,6 +502,17 @@ FROM civicrm_navigation WHERE domain_id = $domainID";
     }
 
     return $url;
+  }
+  
+  /**
+   * Checks if the given URL is not fully-formed
+   *
+   * @param string $url
+   *
+   * @return bool
+   */
+  private static function isNotFullyFormedUrl($url) {
+    return substr($url, 0, 4) !== 'http' && $url[0] !== '/' && $url[0] !== '#';
   }
 
   /**

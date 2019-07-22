85fdac73c81f7a84316e76a780e7563cbde85581
yashodha <yashodha@cividesk.com>
2019-07-22 13:53:42 +0530

    fix comparing mimetype check

--- CRM/Core/Page/File.php
+++ CRM/Core/Page/File.php
@@ -73,9 +73,19 @@ class CRM_Core_Page_File extends CRM_Core_Page {
       CRM_Core_Error::statusBounce('Could not retrieve the file');
     }
 
-    $testMimeType = CRM_Utils_File::getMimeType($path);
-    if ($testMimeType != $mimeType) {
-      throw new CRM_Core_Exception("Supplied Mime Type does not match file Mime Type");
+    $passedInMimeType = CRM_Utils_Request::retrieveValue('mime-type', 'String', $mimeType, FALSE);
+    if (!empty($mimeType) && !empty($passedInMimeType)) {
+      if ($passedInMimeType != $mimeType) {
+	throw new CRM_Core_Exception("Supplied Mime Type does not match file Mime Type");
+      }
+    }
+    elseif (!empty($passedInMimeType)) {
+      $testMimeType = CRM_Utils_File::getMimeType($path);
+      if ($testMimeType != $passedInMimeType) {
+	throw new CRM_Core_Exception("Supplied Mime Type does not match file Mime Type");
+      }
+      // Now that we have ensured that the mime-type matches to what we believe is the mime-type of the file
+      $mimeType = $passedInMimeType;
     }
 
     $buffer = file_get_contents($path);

dec4aaa92eca0756263152233244b57a8d2585a7
root <root@rudra.ops.cividesk.com>
2019-12-11 19:02:50 +0000

    Reverting List-Unsubscribe by URL due to encoding error

--- CRM/Mailing/BAO/Mailing.php
+++ CRM/Mailing/BAO/Mailing.php
@@ -999,7 +999,7 @@ ORDER BY   civicrm_email.is_bulkmail DESC
       'Return-Path' => $verp['bounce'],
       'From' => "\"{$this->from_name}\" <{$this->from_email}>",
       'Subject' => $this->subject,
-      'List-Unsubscribe' => "<{$urls['unsubscribeUrl']}>",
+      'List-Unsubscribe' => "<mailto:{$verp['unsubscribe']}>",
     );
     self::addMessageIdHeader($headers, 'm', $job_id, $event_queue_id, $hash);
     if ($isForward) {

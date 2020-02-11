2c8367c088c7e2ce4af563101425009321d45495
sushantpaste <sushantpaste@gmail.com>
2020-02-11 18:51:23 +0530

    Membership test fix

--- CRM/Price/BAO/PriceFieldValue.php
+++ CRM/Price/BAO/PriceFieldValue.php
@@ -162,7 +162,7 @@ class CRM_Price_BAO_PriceFieldValue extends CRM_Price_DAO_PriceFieldValue {
     $fieldValueDAO->find();
     while ($fieldValueDAO->fetch()) {
       // Do not Generate Price field option for disabled membership Type
-      if ($fieldValueDAO->membership_type_id && ! array_key_exists($fieldValueDAO->membership_type_id, $membershipType)) {
+      if (! isset($fieldValueDAO->membership_type_id) && ! array_key_exists($fieldValueDAO->membership_type_id, $membershipType)) {
         continue;
       }
       CRM_Core_DAO::storeValues($fieldValueDAO, $values[$fieldValueDAO->id]);

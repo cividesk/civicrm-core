82410ed1cfcc910cf8f67681f427bc93a2f295c3
yashodha <yashodha@cividesk.com>
2020-03-19 16:26:07 +0530

    add hook for report view links

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -1559,6 +1559,7 @@ class CRM_Report_Form extends CRM_Core_Form {
       unset($actions['report_instance.csv']);
     }
 
+    CRM_Utils_Hook::links('view.report.links', NULL, $instanceId, $actions);
     return $actions;
   }
 

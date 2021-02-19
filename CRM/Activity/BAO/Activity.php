d373f335b407c1975b30a0c4fcdb8494d7d7221b
yashodha <yashodha@cividesk.com>
2021-02-19 20:32:28 +0530

    add hook to manipulate activity rows

--- CRM/Activity/BAO/Activity.php
+++ CRM/Activity/BAO/Activity.php
@@ -2710,6 +2710,7 @@ INNER JOIN  civicrm_option_group grp ON (grp.id = option_group_id AND grp.name =
       }
     }
 
+    CRM_Utils_Hook::rows($contactActivities);
     $activitiesDT = [];
     $activitiesDT['data'] = $contactActivities;
     $activitiesDT['recordsTotal'] = $params['total'];

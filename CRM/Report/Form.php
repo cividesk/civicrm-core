a852fd491e0d4a3af8263db599b12d0827a8eaf2
yashodha <yashodha@cividesk.com>
2021-11-01 21:05:06 +0530

    add hook to manipute column groups in reports

--- CRM/Report/Form.php
+++ CRM/Report/Form.php
@@ -1257,6 +1257,7 @@ class CRM_Report_Form extends CRM_Core_Form {
       $this->assign('tabs', $this->tabs);
     }
 
+    CRM_Utils_Hook::alterReportVar('colGroups', $colGroups, $this);
     $this->assign('colGroups', $colGroups);
   }
 

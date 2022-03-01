e254eda91bc0477447e98e49d95c9e5c37a5a3fa
Seamus Lee <seamuslee001@gmail.com>
2022-03-01 14:32:28 +1100

    security/core#112 Fix viewing contributions when user doesn't have acess to civicontribute or edit contributions permissions

--- CRM/Contribute/Form/ContributionView.php
+++ CRM/Contribute/Form/ContributionView.php
@@ -41,6 +41,19 @@ class CRM_Contribute_Form_ContributionView extends CRM_Core_Form {
    */
   public function preProcess() {
     $id = $this->get('id');
+    // Check permission for action.
+    if (!CRM_Core_Permission::checkActionPermission('CiviContribute', $this->_action)) {
+      CRM_Core_Error::statusBounce(ts('You do not have permission to access this page.'));
+    }
+
+    $pledgePaymentId = CRM_Core_DAO::getFieldValue('CRM_Pledge_DAO_PledgePayment',
+      $id,
+      'id',
+      'contribution_id'
+    );
+
+    $this->assign('pledgePaymentId', $pledgePaymentId);
+
     $params = ['id' => $id];
     $context = CRM_Utils_Request::retrieve('context', 'Alphanumeric', $this);
     $this->assign('context', $context);
